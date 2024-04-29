5a7767d8259a8b03157678c4e9e8da55bab91c6d
Sayid <sayidisayev6@gmail.com>
2024-04-29 10:22:56 +0500

    Add export

--- lib/download_manager.dart
+++ lib/download_manager.dart
@@ -1,1 +1,5 @@
-class DownloadManager {}
+library download_manager;
+
+export './src/providers/hls_downloader_provider.dart';
+export './src/providers/local_hls_movie_provider.dart';
+export './src/providers/local_hls_movies_provider.dart';
