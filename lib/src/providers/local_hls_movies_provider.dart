a0a0bf54cc1b27623b58c0d958bfb2143d93fead
Daler <108223768+oldHarvester@users.noreply.github.com>
2024-05-20 17:16:04 +0500

    sort tested and works

--- lib/src/providers/local_hls_movies_provider.dart
+++ lib/src/providers/local_hls_movies_provider.dart
@@ -81,28 +81,6 @@ class LocalHlsMoviesNotifier extends AsyncNotifier<List<LocalHlsModel>> {
     if (!state.hasValue) {
       return [];
     }
-    // return state.value!.where(
-    //   (hls) {
-    //     return hls.localHlsState is LocalHlsDownloadingState ||
-    //         hls.localHlsState is LocalHlsInQueueState ||
-    //         hls.localHlsState is LocalHlsPauseState ||
-    //         hls.localHlsState is LocalHlsErrorState;
-    //   },
-    // ).toList()
-    //   ..sort(
-    //     (a, b) {
-    //       if (a.localHlsState is LocalHlsDownloadingState) {
-    //         return -1;
-    //       } else if (a.localHlsState is! LocalHlsInQueueState) {
-    //         return 1;
-    //       } else {
-    //         return a.downloadStatus.creationDate.millisecondsSinceEpoch
-    //             .compareTo(
-    //           b.downloadStatus.creationDate.millisecondsSinceEpoch,
-    //         );
-    //       }
-    //     },
-    //   );
     return state.value!.where(
       (hls) {
         return hls.localHlsState is LocalHlsDownloadingState ||
