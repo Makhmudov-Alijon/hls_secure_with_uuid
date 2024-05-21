3079666d37a700b580db44dcaa7803c24a85c454
Sayid <sayidisayev6@gmail.com>
2024-05-21 11:12:37 +0500

    Change return

--- lib/src/utils/security/security.dart
+++ lib/src/utils/security/security.dart
@@ -36,7 +36,7 @@ class SecurityService extends Security {
     final $2 = token.split('.')[2].split('').reversed.join().substring(0, 27);
     final $3 = key.substring(key.length - 9);
 
-    return $1 + $2 + $3;
+    return '${$1}${$2}${$3}';
   }
 
   @override
